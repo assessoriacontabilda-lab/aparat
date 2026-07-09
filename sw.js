d9cf404970bbe0c0507ad8bcf5a213ff300610fb
assessoriacontabilda-lab <assessoriacontabil.da@gmail.com>
2026-07-09 09:24:24 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'aparat-v3';
+const ASSETS = ['./', 'index.html', 'manifest.json', 'brasao-aparat.png', 'logo-aparat.png', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.mode === 'navigate' || req.destination === 'document') {
+    e.respondWith(
+      fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put('index.html', copy));
+        return res;
+      }).catch(() => caches.match('index.html'))
+    );
+    return;
+  }
+  e.respondWith(caches.match(req).then(cached => cached || fetch(req)));
+});
